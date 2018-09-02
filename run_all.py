7eae8a0f0dc2d5e213ea690dd2237ee1518de07f
bacross <bacross@hotmail.com>
2018-09-02 15:20:15 -0400

    fixed position of run_all relative to file tree

--- run_all.py
+++ run_all.py
@@ -4,7 +4,6 @@ warnings.filterwarnings("ignore")
 ### load cfg file
 from src import cfg
 
-
 ### Refresh Radon data if flag==True, otherwise load from saved parquet file
 from src.etl.get_load_radon_dat import get_load_radon_dat as glrd
 
@@ -16,8 +15,16 @@ from src.etl.proc_radon import proc_radon
 data =  proc_radon(df_radon)
 
 ### Run pymc3 hier regression
-from src.models.models_pymc3 import hier_reg as hr
+from src.models.models_pymc3 import hier_reg_pymc3 as hr_pymc3
 
-time_del = hr.run_hier_reg(data, cfg.sample_size, cfg.njobs)
+time_del_pm = hr_pymc3.run_hier_reg(data, cfg.sample_size, cfg.njobs)
 
-print('Time Delta for Hierarchical Regression Using pymc3 is %s seconds' %(time_del))
+print('Time Delta for Hierarchical Regression Using pymc3 is %s seconds' %(time_del_pm))
+
+### Run pystan hier regression 
+from src.models.models_pystan import hier_reg_pystan as hr_stan
+
+time_del_stan = hr_stan.run_hier_reg(data, cfg.sample_size, cfg.njobs, cfg.hier_stan_code)
+
+print('Time Delta for Hierarchical Regression Using pystan is %s seconds' %(time_del_stan))
+
